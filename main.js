c88a7e00d8bd7b4dad985f69d6179e134cbbd18a
Student <michael@avdvco.com>
2015-06-30 11:23:21 -0600

    first commit

---
+++ main.js
@@ -0,0 +1,20 @@
+$(document).on('ready', function() {
+  
+var animals = ['rat', 'cat', 'butterfly', 'marmot', 'ocelot'];
+
+// for (var i = 0; i < animals.length; i++) {
+// 	console.log(animals[i]);
+// };
+
+
+// // PROBLEMS:
+// 1. rat cat butterfly marmot
+// 2. rat butterfly ocelot
+// 3. ocelot marmot butterfly cat rat
+// 4. rat cat cat butterfly butterfly marmot marmot ocelot
+
+
+var firstFour  = function(animals){
+	return.animals[4];
+}
+});
